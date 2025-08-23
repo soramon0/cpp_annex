5fbe4cb7474a04c7a1dacc030346b70a041271fd
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-08-23 20:10:02 +0100

    mod00/ex00

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cpp_annex
+# Learning C++
---
+++ mod00/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+#include <cstring>
+#include <iostream>
+
+char *str_toupper(char *str) {
+  for (int i = 0; str[i]; ++i) {
+    str[i] = std::toupper(static_cast<unsigned char>(str[i]));
+  }
+  return str;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    std::cout << str_toupper(argv[i]);
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
