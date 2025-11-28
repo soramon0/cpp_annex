2e5b48d53d2909a276fd877c9765e0d181da890b
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-11-28 05:22:23 +0100

    fix: folder structure

---
+++ mod02/ex00/Fixed.cpp
@@ -0,0 +1,30 @@
+#include "./Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : _value(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+  std::cout << "Copy constructor called" << std::endl;
+  this->_value = other._value;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called" << std::endl;
+
+  if (this != &other) {
+    this->_value = other.getRawBits();
+  }
+
+  return *this;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return this->_value;
+}
+
+void Fixed::setRawBits(int const raw) { this->_value = raw; }
--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -1,20 +1,10 @@
 #include "./Fixed.hpp"
-#include <iostream>
 
-const int Fixed::_fractions = 8;
+Fixed::Fixed() : _value(0) {}
 
-Fixed::Fixed() : _value(0) {
-  std::cout << "Default constructor called" << std::endl;
-}
-
-Fixed::Fixed(const Fixed &other) {
-  std::cout << "Copy constructor called" << std::endl;
-  (*this) = other;
-}
+Fixed::Fixed(const Fixed &other) { this->_value = other._value; }
 
 Fixed &Fixed::operator=(const Fixed &other) {
-  std::cout << "Copy assignment operator called" << std::endl;
-
   if (this != &other) {
     this->_value = other.getRawBits();
   }
@@ -22,11 +12,8 @@ Fixed &Fixed::operator=(const Fixed &other) {
   return *this;
 }
 
-Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+Fixed::~Fixed() {}
 
-int Fixed::getRawBits(void) const {
-  std::cout << "getRawBits member function called" << std::endl;
-  return this->_value;
-}
+int Fixed::getRawBits(void) const { return this->_value; }
 
 void Fixed::setRawBits(int const raw) { this->_value = raw; }
