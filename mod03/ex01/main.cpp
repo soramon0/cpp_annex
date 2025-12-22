431bfbac67e7abf3f6cf0f163d3ad243d6321add
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-12-22 21:10:30 +0100

    refactor: enhance output readability in main.cpp with additional separators

--- mod03/ex01/main.cpp
+++ mod03/ex01/main.cpp
@@ -2,19 +2,23 @@
 #include <iostream>
 
 int main() {
+  std::cout << "---------" << std::endl;
   ClapTrap clappy("sora");
   std::cout << clappy << std::endl;
   clappy.attack("Hero");
-  
+  std::cout << "---------" << std::endl;
+
+  std::cout << "---------" << std::endl;
   ScavTrap scav("Roboto");
   std::cout << scav << std::endl;
   scav.attack("Enemy");
   scav.guardGate();
-  std::cout << scav << std::endl;
-  
+  std::cout << "---------" << std::endl;
   {
-    ScavTrap test("TestBot");
+    std::cout << "---------" << std::endl;
+    ScavTrap test("TestTrap");
+    std::cout << test << std::endl;
   }
-  
+
   return 0;
 }
