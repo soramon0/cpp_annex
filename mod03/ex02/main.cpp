bb00e4da571dd9793ad6d4354292cc2f7ec3497f
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-12-22 21:05:03 +0100

    refactor: add separators in main.cpp for better output readability

--- mod03/ex02/main.cpp
+++ mod03/ex02/main.cpp
@@ -2,19 +2,25 @@
 #include <iostream>
 
 int main() {
+  std::cout << "---------" << std::endl;
   ClapTrap clappy("sora");
   std::cout << clappy << std::endl;
   clappy.attack("Hero");
-  
+  std::cout << "---------" << std::endl;
+
+  std::cout << "---------" << std::endl;
   FragTrap frag("FragBot");
   std::cout << frag << std::endl;
   frag.attack("Target");
   frag.highFivesGuys();
   std::cout << frag << std::endl;
-  
+  std::cout << "---------" << std::endl;
+
   {
+    std::cout << "---------" << std::endl;
     FragTrap test("TestFrag");
+    std::cout << test << std::endl;
   }
-  
+  std::cout << "---------" << std::endl;
   return 0;
 }
